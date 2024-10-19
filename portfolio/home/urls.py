549cd4d5e4fd073c07c0b158eaff529b2f2f70aa
DEEPAK  KUMAR <dkumardk002@gmail.com>
2024-10-19 12:23:45 +0530

    commit for basic template setup

--- portfolio/home/urls.py
+++ portfolio/home/urls.py
@@ -4,6 +4,14 @@ from .views import *
 
 urlpatterns = [
     path("",index,name="index"),
-    path("home/",home,name="home"),
-
+    path("about/",about,name="about"),
+    path("blog-details/",blogDetails,name="blog-details"),
+    path("blog/",blog,name="blog"),
+    path("contact/",contact,name="contact"),
+    path("portfolio-details/",portfolioDetails,name="portfolio-details"),
+    path("portfolio/",portfolio,name="portfolio"),
+    path("pricing/",pricing,name="pricing"),
+    path("services/",services,name="services"),
+    path("team/",team,name="team"),
+    path("testimonials/",testimonials,name="home"),
 ]
--- portfolio/home/views.py
+++ portfolio/home/views.py
@@ -2,7 +2,56 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request,"index.html")
+    context = {}
+    context['indexClass'] = 'active'
+    return render(request,"index.html", context = context)
 
-def home(request):
-    return render(request,"home.html")
+def about(request):
+    context = {}
+    context['aboutClass'] = 'active'
+    return render(request,"about.html", context = context)
+
+def blogDetails(request):
+    context = {}
+    context['indexClass'] = 'active'
+    return render(request,"blog-details.html", context = context)
+
+def blog(request):
+    context = {}
+    context['blogClass'] = 'active'
+    return render(request,"blog.html", context = context)
+
+def contact(request):
+    context = {}
+    context['contactClass'] = 'active'
+    return render(request,"contact.html", context = context)
+
+def portfolioDetails(request):
+    context = {}
+    context['indexClass'] = 'active'
+    return render(request,"portfolio-details.html", context = context)
+
+def portfolio(request):
+    context = {}
+    context['portfolioClass'] = 'active'
+    return render(request,"portfolio.html", context = context)
+
+def pricing(request):
+    context = {}
+    context['pricingClass'] = 'active'
+    return render(request,"pricing.html", context = context)
+
+def services(request):
+    context = {}
+    context['servicesClass'] = 'active'
+    return render(request,"services.html", context = context)
+
+def team(request):
+    context = {}
+    context['teamClass'] = 'active'
+    return render(request,"team.html", context = context)
+
+def testimonials(request):
+    context = {}
+    context['indexClass'] = 'active'
+    return render(request,"testimonials.html", context = context)
