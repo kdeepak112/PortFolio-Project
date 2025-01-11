200476e2fbb279adceb2f3896037a335539a1326
kdeepak112 <dkumardk002@gmail.com>
2025-01-11 16:44:15 +0530

    commit for changes in sqllite an deployment

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -77,22 +77,22 @@ WSGI_APPLICATION = "portfolio.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql", 
-        "NAME": "portfolioDB",                    
-        "USER": "postgres",                    
-        "PASSWORD": "password@123",            
-        "HOST": "localhost",                        
-        "PORT": "5432",                            
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql", 
+#         "NAME": "portfolioDB",                    
+#         "USER": "postgres",                    
+#         "PASSWORD": "password@123",            
+#         "HOST": "localhost",                        
+#         "PORT": "5432",                            
+#     }
+# }
 
 
 # Password validation
