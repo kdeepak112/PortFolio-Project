abf0ba60f701cfd78d5e5e323254ff2185015263
kdeepak112 <dkumardk002@gmail.com>
2025-01-11 18:00:03 +0530

    commit for req

--- portfolio/portfolio/urls.py
+++ portfolio/portfolio/urls.py
@@ -23,4 +23,8 @@ from django.conf import settings
 urlpatterns = [
     path("",include("home.urls")),
     path("admin/", admin.site.urls),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
